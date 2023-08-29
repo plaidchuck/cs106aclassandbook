60e8a42cf068bede4b2e609fda08b19402273bbb
plaidchuck <142672627+plaidchuck@users.noreply.github.com>
2023-08-29 13:19:09 -0400

    Program to calculate 3.14 to the -4 through 4th power

--- MiscJunk.java
+++ MiscJunk.java
@@ -1,44 +1,42 @@
-/* this program displays all 10 verses
-* for the children's rhyme "This Old Man"
-* using a "for loop" to increment each verse
-* and uses a "switch statement" to plug in a word
-* that rhymes with the specific verse number
+/* 
+* This program will take the floating point number pi (3.14)
+* and return the calculation of the powers of it from -4 to 4.
 */
- 
+
+//imports standard library
 import acm.program.*;
  
- 
+//sets class and extends for console ouput 
 public class MiscJunk extends ConsoleProgram {
-     
-    public void run () {
+   
+	//sets private method to with a double and integer variable
+	private double raiseToIntPower(double n , int k) {
+		//sets if loop if k parameter is negative
+		if (k < 0) {
+		//gives double variable of 1 divided by n to a negative power
+		double result = (1 / (Math.pow(n, k)));
+		//sends calculated result variable to main run method
+		return result;
+		//if k is not negative, just calculates n to the k power and
+		//returns result to main run method
+		} else {
+			double result = (Math.pow(n, k));
+			return result;
+		}
+	}
+    
+	//main run method
+	public void run () {
+        //sets n double variable to pi (3.14) 
+		double n = 3.14;
+        //sets for loop to run from -4 to 4 via the index variable
+		//and uses this variable for the power argument to send to raiseToIntPower
+		for (int i = -4; i < 5; i++) {
+			//prints returned double variable result of the 
+			//method after sending n and i
+			println(raiseToIntPower(n, i));
+         }
          
-        /*"for loop" used to increment the verse numbering by using i*/
-         
-        for (int i = 1; i<= 10; i++) {
-        	
-        	 println("This old man, he played " + i + ".  ");             
-             
-                 
-        /*"switch statement" used to plug in a rhyming word for each specific verse
-          using i*/  
-                    switch (i) {
-                        case 1: println ("He played knick-knack on my thumb." );  break;
-                        case 2: println ("He played knick-knack on my shoe." );  break;
-                        case 3: println ("He played knick-knack on my knee." );  break;
-                        case 4: println ("He played knick-knack on my door." ); break;
-                        case 5: println ("He played knick-knack on my hive." ); break;
-                        case 6: println ("He played knick-knack on my sticks." ); break;
-                        case 7: println ("He played knick-knack on my heaven." );  break;
-                        case 8: println ("He played knick-knack on my gate." ); break;
-                        case 9: println ("He played knick-knack on my spine." ); break;
-                        case 10: println ("He played knick-knack on my pen." ); break;
-                    }
-        /*the remaining lines of the rhyme*/   
-                    println ("With a knick-knack, paddy-whack." );
-                    println ("Give your dog a bone." );
-                    println ("This old man came rolling home.");    
-                    println ("  ");
-       
-        }
+        
     }
 }
