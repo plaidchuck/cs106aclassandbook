6c1737eb82d5256b3f74c33089f9c732f1a63f4e
plaidchuck <142672627+plaidchuck@users.noreply.github.com>
2023-10-08 14:39:04 -0400

    Update PacMan.java
    Update code to remove redundant for loops.

--- Ch9/PacMan.java
+++ Ch9/PacMan.java
@@ -9,55 +9,76 @@ import java.awt.*;
 public class PacMan extends GraphicsProgram {
 
 /*
- * Constants for PacMan's size as a GArc, movement speed, and animation delay
+ * Constants for PacMan's size as a GArc, movement speed, animation delay,
+ * and initial starting X position.
  */
-private static final double PAC_DIAM = 80;
-private static final double SPEED = 10;
+private static final int PAC_DIAM = 80;
+private static final int SPEED = 10;
 private static final int DELAY = 50;
+private static final double START_X = 0;
+
+
 
 	public void run() {
-		createPacMan();
-	}
-/*
- * Create PacMan object as a filled yellow GArc with 90 degree open mouth,
- * calculate by the start angle being 45 degrees and the sweep angle being 270
- * degrees.
- */
-	private void createPacMan() {	
-	//Set Variables for centering PacMan on canvas
-		double startX = 0;
-		double startY = (getHeight() - PAC_DIAM) / 2;
-			
-	//Offsets to start and sweep angles to close PacMans mouth
-		double startAngle = 45;
-		double sweepAngle = 270;
 		
+//Canvas color for more contrast with yellow Pacman
 		setBackground(Color.BLACK);
 		
-		GArc pacMan = new GArc(PAC_DIAM, PAC_DIAM, startAngle , sweepAngle);
-		pacMan.setFilled(true);
-		pacMan.setColor(Color.BLACK);
-		pacMan.setFillColor(Color.YELLOW);
-		add (pacMan, startX, startY);
-			while (pacMan.getX() < getWidth()) {
-				for (int i = 0; i < 4; i++) {
-					startAngle = 45;
-					sweepAngle = 270;
-					pacMan.setStartAngle(startAngle - (i * 15));
-					pacMan.setSweepAngle(sweepAngle + (i * 30));
-					pacMan.move(SPEED, 0);
-					pause(DELAY);
+		drawPacman();
+
+//Moves Pacman far until the object is past the right edge of canvas
+			while (pacman.getX() < getWidth()) {
+				
+/* 
+ * Sets two conditional loops based on if the "mouth" is open or closed.
+ * In either scenario the start and sweep variables are adjusted to
+ * open and close the mouth respectively and Pacman is moved
+ * across the screen.
+ */
+				if (start < 0) {
+					start += 15;
+					sweep -= 30;
+					movePacman();
 				}
 				
-				for (int i = 0; i < 4; i++) {
-					startAngle = 0;
-					sweepAngle = 360;
-					pacMan.setStartAngle(startAngle + (i * 15));
-					pacMan.setSweepAngle(sweepAngle - (i * 30));
-					pacMan.move(SPEED, 0);
-					pause(DELAY);
-				} 
-			}
-	
+				if (start == 0) {
+					while (start > -45) {
+					start -= 15;
+					sweep += 30;
+					movePacman();
+					}
+				}
+			} 
 	}
+
+/*
+ * Centers Pacman on the screen vertically and places him horizontally
+ * and sets the appropriate size based on constants.
+ */
+	private void drawPacman() {
+		double startY = (getHeight() - PAC_DIAM) / 2;
+		pacman = new GArc(START_X, startY, PAC_DIAM, PAC_DIAM, start, sweep);
+		pacman.setFilled(true);
+		pacman.setFillColor(Color.YELLOW);
+		add(pacman);
+		pause(DELAY);
+	}
+
+/*
+ * Moves Pacman the number of pixels set by constant. Also sets current start
+ * and sweep angle of mouth and then pauses for the time specified by
+ * constant.
+ */
+	private void movePacman() {
+		pacman.move(SPEED, 0);
+		pacman.setStartAngle(start);
+		pacman.setSweepAngle(sweep);
+		pause(DELAY);
+		
+	}
+	
+// instance variables
+	private GArc pacman;
+	private int start = -45;		//GArc start angle for Pacman's mouth
+	private int sweep = -270;	//GArc sweep angle for Pacman's mouth
 }
