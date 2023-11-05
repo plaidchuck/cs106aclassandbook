13eb7d62a06b6e69975c17c7495c5dad17374f66
plaidchuck <142672627+plaidchuck@users.noreply.github.com>
2023-11-05 14:01:06 -0500

    Update PigLatin.java
    Updated version that doesn't use any string builder or tokenizer, and also capitalizes the translated words properly.

--- Ch8/PigLatin.java
+++ Ch8/PigLatin.java
@@ -4,43 +4,58 @@
  */
 
 import acm.program.*;
-import java.util.*;
+//import java.util.*;
 
 public class PigLatin extends ConsoleProgram {
 	
-private static final String DELIMITERS = "!@#$%^&*()[]\\;',./-=_+{}|:<>?`~ ;\" +";
+//private static final String DELIMITERS = "!@#$%^&*()[]\\;',./-=_+{}|:<>?`~;\"+";
 	public void run() {
 
 //state program purpose
 		println("This program translates a text line into Pig Latin.");
-//Receive input of text line into String variable
-		String textLine = readLine("Enter a line of text: ");
+//Keep requesting input and translating the line
+		while (true) {
+			String textLine = readLine("Enter a line of text: ");
 //Output translated line
-		println(translationLine(textLine));
+			println(translationLine(textLine));
+			println("");
+		}
 	}
 
 /*
- * Turn the inputted line into String tokens and check that each word
- * has no characters in it, if so translate the word into pig latin.
+ * Take inputed string  line and check each character until a space
+ * is encountered to signify the end of a word. Take each word
+ * and translate it into pig latin with a space between the words then
+ * return the word.
  */
 	private String translationLine(String line) {
 		String result = "";
-		StringTokenizer tokenizer =
-		  new StringTokenizer(line, DELIMITERS, true);
-		while (tokenizer.hasMoreTokens()) {
-			String token = tokenizer.nextToken();
-			if (isWord(token)) {
-				token = translateWord(token);
+		int start = 0;
+		for (int i = 0; i < line.length(); i++) {
+			
+			if (Character.isWhitespace(line.charAt(i))) {
+				String word = line.substring(start, i);
+				
+				result += translateWord(word) + " ";
+				start = i + 1;
 			}
-			result += token;
+
 		}
-		return result;
+	
+//Translates the last word of the line
+		if (start < line.length()) {
+			String word = line.substring(start);
+			result += translateWord(word);
+		}
+
+		return result.trim();
 	}
 	
+
+
 /*
- * Check that each character in the token is an actual letter.
- * If it isn't, the world is left alone and outputted as it was
- * originally.
+ * Check that the word subtring is an actual word with no
+ * specical characters in it.
  */
 	
 	private boolean isWord(String token) {
@@ -52,19 +67,81 @@ private static final String DELIMITERS = "!@#$%^&*()[]\\;',./-=_+{}|:<>?`~ ;\" +
 		return true;
 	}
 	
+/*
+ * Translates a word into pig latin by finding the first vowel and
+ * changing word based on if the first letter is a vowel or not.
+ * Also returns any word that contains special characters or no vowels
+ * except in the case where the word contains a special character at the
+ * end of the word(punctuation). In that case, the character is placed into
+ * a char variable and added to the end of the translated word. Also
+ * check if word is capitalized so the actual pig latin word starts with
+ * an uppercase letter as well.
+ */
 	private String translateWord(String word) {
+		String result = "";
+		char ch = ' ';
+		if (!Character.isLetterOrDigit(word.charAt(word.length() - 1))) {
+		    ch = word.charAt(word.length() - 1);
+		    word = word.substring(0, word.length() - 1);
+		}
 		int vp = findFirstVowel(word);
-		if (vp == -1) {
-			return word;
+		if (vp == -1 || !isWord(word)) {
+			return word + ch;
 		} else if (vp == 0) {
-			return word + "way";
+			if (ch != ' ' && startsWithUppercase(word)) return capitalize(word) + "way" + ch;
+			else if (ch != ' ') return word + "way" + ch;
+			else if (startsWithUppercase(word)) {
+				result = word + "way";
+				return capitalize(result);
+			}
+			else return word + "way";
 		} else {
 			String head = word.substring(0, vp);
 			String tail = word.substring(vp);
-			return tail + head + "ay";
+			if (ch != ' ' && startsWithUppercase(word)){
+				result = tail.trim() + head.trim() + "ay" + ch;
+				return capitalize(result);
+			} else if (ch != ' ') {
+				result = tail.trim() + head.trim() + "ay" + ch;
+				return result;
+			} else if (startsWithUppercase(word)){
+				result = tail.trim() + head.trim() + "ay";
+				return capitalize(result);
+			} else {
+				result = tail.trim() + head.trim() + "ay";
+				return result;
+			}
 		}
 	}
 	
+	private boolean startsWithUppercase(String word) {
+		for (char ch = 'A'; ch <= 'Z'; ch++) {
+			if (word.charAt(0) == ch) {
+				return true;
+			}
+		}
+		return false;
+			
+	}
+	
+	/*
+	 * Returns a string that capitalizes the first letter in a string
+	 * and makes all the others lowercase if necessary. Does not affect non
+	 * letter characters.
+	 */
+		private String capitalize(String str) {
+			String result = "";
+			result = result + Character.toUpperCase(str.charAt(0));
+			for(int i = 1 ; i < str.length(); i++) {
+				result = result + Character.toLowerCase(str.charAt(i));
+				}
+			return result;
+		}
+	
+	/*
+	 * Helper method to identify location of the first vowel in
+	 * a string
+	 */
 	private int findFirstVowel(String word) {
 		for (int i = 0; i < word.length(); i ++) {
 			if (isEnglishVowel(word.charAt(i))) return i;
@@ -72,6 +149,10 @@ private static final String DELIMITERS = "!@#$%^&*()[]\\;',./-=_+{}|:<>?`~ ;\" +
 		return -1;
 	}
 	
+	/*
+	 * Helper method to determine if a character is a standard English
+	 * vowel.
+	 */
 	private boolean isEnglishVowel(char ch) {
 		switch (Character.toLowerCase(ch)) {
 		case 'a': case 'e': case 'i': case 'o': case 'u':
