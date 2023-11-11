a28892e592dc00395cf2695691e60c4d639a9dac
plaidchuck <142672627+plaidchuck@users.noreply.github.com>
2023-11-10 23:03:06 -0500

    Update StringBlockinator.java
    Revision that returns blocks between whitespaces but also ignores leading and trailing white space in the string and consecutive white space inside the string between blocks

--- Ch8/StringBlockinator.java
+++ Ch8/StringBlockinator.java
@@ -1,44 +1,62 @@
-import acm.program.*;
+
 
 public class StringBlockinator {
 
     public StringBlockinator(String inputLine) {
         this.inputLine = inputLine;
-        index = 0;
+        startIndex = 0;
         delims = null; //set so that the only delimiter is white space
     }
 
     public StringBlockinator(String inputLine, String delims) {
         this.inputLine = inputLine;
         this.delims = delims;
-        index = 0;
+        startIndex = 0;
     }
 
     public String nextBlock() {
-
+   
+    	inputLine = inputLine.trim();
+    	boolean inDelimiterRun = false;
+    	int endIndex = 0;
     	
-    	for (int i = index; i < inputLine.length(); i++) {
+    	for (int i = startIndex; i < inputLine.length(); i++) {
     		char inputChar = inputLine.charAt(i);
     		
-    		if (Character.isWhitespace(inputChar)) {
- 
-    			block = inputLine.substring(index, i);
-    			index = i + 1;
-    			return block;
+    		if (Character.isWhitespace(inputChar) && !inDelimiterRun) {
+    			endIndex = i;
+    			inDelimiterRun = true;
+    			
+    			if (startIndex < endIndex) {
+    				block = inputLine.substring(startIndex, endIndex);
+    				startIndex = endIndex;
+    				return block;
+    			}
+    			
     		}
+    		
+    		if (!Character.isWhitespace(inputChar) && inDelimiterRun) {
+    			
+    			inDelimiterRun = false;
+    			startIndex = i;
+  
+    		}
+    		
+    	}
+  
+    	
+    	if (startIndex < inputLine.length()) {
+    		block = inputLine.substring(startIndex);
+    		startIndex = inputLine.length();
+    		return block;
     	}
     	
-    	if (index < inputLine.length()) {
-    		block = inputLine.substring(index);
-			index = inputLine.length();
-			return block;
-    	} 
     	else return null;
-    }
+     }
      
     
     public boolean hasMoreBlocks() {
-        return index < inputLine.length();
+        return startIndex < inputLine.length();
     }
 
     private boolean isDelimiter(char ch) {
@@ -51,6 +69,5 @@ public class StringBlockinator {
 	private String inputLine; //holds original input string
 	private String delims; //holds specified delimiters for blocks
 	private String block; //holds current block from original String
-	private int index; //holds index position during string loops
+	private int startIndex; //holds start index position during string loops
 }
-
